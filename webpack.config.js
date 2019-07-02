2b85299240894bba9c6e387a5d96c86e5c79d8c3
kozyrev-artem <dinta666@rambler.ru>
2019-07-02 18:08:15 +0300

    webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/js/jquery.main.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
